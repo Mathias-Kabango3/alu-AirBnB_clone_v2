02c42a3fc9203e15a4414def49343c211ec117f9
Mathias-kabango3 <m.kabango@alustudent.com>
2024-09-28 17:58:22 +0000

    orm setup

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+""" State Module for HBNB project """
+import os
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-# from sqlalchemy.orm import relationship
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class """
+    """Amenities of a place"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    # if storage_type == 'db':
-    #     place_amenities = relationship("Place", secondary="place_amenity")
-    # else:
-    #     name = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary="place_amenity")
